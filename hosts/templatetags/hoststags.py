c6fdd18171c5a86c2f7d8c941738520d5e40b2a7
DanG <44029238+dangulica@users.noreply.github.com>
2019-10-25 15:38:04 +0300

    added updated comments

--- hosts/templatetags/hoststags.py
+++ hosts/templatetags/hoststags.py
@@ -8,29 +8,21 @@ register = template.Library()
 def next(some_list, current_index):
     """
     Returns the next element of the list using the current index if it exists.
-    Otherwise returns an empty string.
+    Otherwise returns the same element
     """
     if current_index >= 0 and current_index < len(some_list) - 1:
         return some_list[int(current_index) + 1]  # access the next element
     else:
-        return some_list[int(current_index)]  # return same element in case of exception
-    # try:
-    #     return some_list[int(current_index) + 1]  # access the next element
-    # except:
-    #     return some_list[int(current_index)]  # return same element in case of exception
+        return some_list[int(current_index)]  # return same element if index is out of range
 
 
 @register.filter
 def previous(some_list, current_index):
     """
     Returns the previous element of the list using the current index if it exists.
-    Otherwise returns an empty string.
+    Otherwise returns the same element.
     """
     if current_index >= 1 and current_index < len(some_list):
         return some_list[int(current_index) - 1]  # access the next element
     else:
-        return some_list[int(current_index)]  # return same element in case of exception
-    # try:
-    #     return some_list[int(current_index) - 1]  # access the previous element
-    # except:
-    #     return some_list[int(current_index)]  # return same element in case of exception
+        return some_list[int(current_index)]  # return same element if index is out of range
